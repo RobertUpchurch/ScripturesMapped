a3811d1ee3fba2d3463e66858908ea61215aa20c
Rob Upchurch <robertc.upchurch@gmail.com>
2020-02-04 15:52:10 -0700

    fixed a couple of minor bugs and implemented navigate chapter

--- js/scriptures.js
+++ js/scriptures.js
@@ -35,6 +35,7 @@ const Scriptures = (function () {
     const REQUEST_STATUS_ERROR = 400;
     const TAG_VOLUME_HEADER = "h5";
     const URL_BOOKS = "https://scriptures.byu.edu/mapscrip/model/books.php";
+    const URL_SCRIPTURES = "https://scriptures.byu.edu/mapscrip/mapgetscrip.php";
     const URL_VOLUMES = "https://scriptures.byu.edu/mapscrip/model/volumes.php";
 
     //Private Variables
@@ -47,8 +48,11 @@ const Scriptures = (function () {
     let booksGrid;
     let booksGridContent;
     let cacheBooks;
-    let chaptersContent;
+    let chaptersGrid;
     let chaptersGridContent;
+    let encodedScripturesUrlParamaters;
+    let getScripturesCallback;
+    let getScripturesFailure;
     let htmlAnchor;
     let htmlDiv;
     let htmlElement;
@@ -61,12 +65,12 @@ const Scriptures = (function () {
     let volumesGridContent;
 
     //Private Methods
-    ajax = function (url, successCallback, failureCallback) {
+    ajax = function (url, successCallback, failureCallback, skipJsonParse) {
         let request = new XMLHttpRequest();
         request.open(REQUEST_GET, url, true);
         request.onload = function () {
             if (request.status >= REQUEST_STATUS_OK && request.status < REQUEST_STATUS_ERROR) {
-                let data = JSON.parse(request.response);
+                let data = skipJsonParse ? request.response : JSON.parse(request.response);
                 if (typeof successCallback === "function") {
                     successCallback(data);
                 }
@@ -133,7 +137,7 @@ const Scriptures = (function () {
         }
     };
 
-    chaptersContent = function (book) {
+    chaptersGrid = function (book) {
         return htmlDiv({
             classKey: CLASS_VOLUME,
             content: htmlElement(TAG_VOLUME_HEADER, book.fullName)
@@ -147,7 +151,7 @@ const Scriptures = (function () {
         let gridContent = "";
         let chapter = 1;
 
-        while (chapter < book.numChapters) {
+        while (chapter <= book.numChapters) {
             gridContent += htmlLink({
                 classKey: `${CLASS_BUTTON} ${CLASS_CHAPTER}`,
                 id: chapter,
@@ -158,7 +162,32 @@ const Scriptures = (function () {
             chapter += 1;
         }
 
-        return gridContent
+        return gridContent;
+    };
+
+    encodedScripturesUrlParamaters = function (bookId, chapter, verses, isJst) {
+        if (bookId !== undefined && chapter !== undefined) {
+            let options = "";
+
+            if (verses !== undefined) {
+                options += verses;
+            }
+
+            if (isJst !== undefined) {
+                options += "&jst=JST";
+            }
+
+            return `${URL_SCRIPTURES}?book=${bookId}&chap=${chapter}&verses=${options}`;
+        }
+    };
+
+    getScripturesCallback = function (chapterHtml) {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = chapterHtml;
+        // Needs Work: setupmarkers();
+    };
+
+    getScripturesFailure = function () {
+        console.log("Unable to retrieve chapter content from server.");
     };
 
     htmlAnchor = function (volume) {
@@ -245,13 +274,13 @@ const Scriptures = (function () {
         } else {
             document.getElementById(DIV_SCRIPTURES).innerHTML = htmlDiv({
                 id: DIV_SCRIPTURES_NAVIGATOR,
-                content: chaptersContent(book)
+                content: chaptersGrid(book)
             });
         }
     };
 
-    navigateChapter = function (bookId, chapterId) {
-        console.log(bookId, chapterId);
+    navigateChapter = function (bookId, chapter) {
+        ajax(encodedScripturesUrlParamaters(bookId, chapter), getScripturesCallback, getScripturesFailure, true);
     };
 
     navigateHome = function (volumeId) {
